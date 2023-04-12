d910041278dee08069214eaf0f1f5fe85eb26c41
Panagiotis Tsoukias <panats@microsoft.com>
2023-04-12 11:43:01 +0300

    updated tag script

--- Scripts/Set-Tag.ps1
+++ Scripts/Set-Tag.ps1
@@ -1,4 +1,7 @@
 $tag = @{"FileServerNTFSAudit"="Enabled"}
+$subscriptionId = "870108ec-6747-428d-9627-c2f9db260b7f"
+Set-AzContext -SubscriptionId $subscriptionId
+$resourceGroupName = "Arc-rg"
 
 $machineName = "SQL01"
 $machine = Get-AzResource -ResourceType "Microsoft.HybridCompute/machines" -ResourceGroupName $resourceGroupName -Name $machineName
