b81df9547497026a325f174d9cea7c56cc6fe59e
Panagiotis Tsoukias <panats@microsoft.com>
2023-04-13 12:06:52 +0300

    updated tag script

--- Scripts/Set-Tag.ps1
+++ Scripts/Set-Tag.ps1
@@ -3,6 +3,25 @@ $subscriptionId = "870108ec-6747-428d-9627-c2f9db260b7f"
 Set-AzContext -SubscriptionId $subscriptionId
 $resourceGroupName = "Arc-rg"
 
-$machineName = "SQL01"
-$machine = Get-AzResource -ResourceType "Microsoft.HybridCompute/machines" -ResourceGroupName $resourceGroupName -Name $machineName
-Update-AzTag -ResourceId  $machine.ResourceId -Tag $tag -Operation Merge
+try 
+{
+    $machineNames = Get-Content -Path '.\OMS_Workspace_Servers - NTFS Audit.txt'
+    foreach ($machineName in $machineNames) 
+    {
+        $machine = Get-AzResource -ResourceType "Microsoft.HybridCompute/machines" -ResourceGroupName $resourceGroupName -Name $machineName.split(".")[0] -ErrorAction SilentlyContinue
+        if($machine)
+        {
+            Write-Output "Merging tag on $($machineName.split(".")[0])"
+            $null = Update-AzTag -ResourceId  $machine.ResourceId -Tag $tag -Operation Merge -ErrorAction Stop
+        }
+        else 
+        {
+            Write-Error "$($machineName.split(".")[0]) Not found!"
+        }
+    }
+}
+catch 
+{
+    Write-Error -Message $_.Exception
+    throw $_.Exception
+}
